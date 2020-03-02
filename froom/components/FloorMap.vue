ac49168edc64b9f983aecbca2d2f4e290694ad7e
Ivan Germanov <ivangermanov98@gmail.com>
2020-03-02 22:41:41 +0100

    Add mock map

--- froom/components/FloorMap.vue
+++ froom/components/FloorMap.vue
@@ -1,17 +1,34 @@
 <template>
-  <no-ssr>
-    <l-map />
-  </no-ssr>
+  <div style="height: 100%; padding: 10px;">
+    <no-ssr>
+      <l-map :options="mapOptions">
+        <l-image-overlay :url="url" :bounds="bounds" />
+      </l-map>
+    </no-ssr>
+  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+import { CRS } from 'leaflet'
+import { LMap, LImageOverlay } from 'vue2-leaflet'
+import png from '~/mock/floor.png'
 
 export default Vue.extend({
   components: {
     LMap,
-    LTileLayer,
-    LMarker
+    LImageOverlay
+  },
+  data () {
+    return {
+      mapOptions: {
+        crs: CRS.Simple,
+        minZoom: 2,
+        maxZoom: 4,
+        maxBounds: [[0, 0], [1416, 512]]
+      },
+      url: png,
+      bounds: [[0, 0], [1416, 512]]
+    }
   }
 })
 </script>
