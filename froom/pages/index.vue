3f42c01e1688cffbb6d1eaca2b297dabaafeb013
Ivan Germanov <ivan.germanov@cytosmart.com>
2020-03-02 22:02:12 +0100

    Create initial FloorMap page

--- froom/pages/index.vue
+++ froom/pages/index.vue
@@ -1,10 +1,6 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
       <v-card>
         <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
@@ -51,7 +47,7 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
+          <v-btn color="primary" nuxt to="/floor-map">
             Continue
           </v-btn>
         </v-card-actions>
@@ -61,13 +57,8 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
   components: {
-    Logo,
-    VuetifyLogo
   }
 }
 </script>
