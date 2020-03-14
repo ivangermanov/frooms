141ddcc5fb6eeaa51ee3180dd1d76f197be36cfa
Ivan Germanov <ivangermanov98@gmail.com>
2020-03-14 15:36:00 +0100

    Add proper z-index to map

--- froom/components/FloorMap.vue
+++ froom/components/FloorMap.vue
@@ -1,11 +1,12 @@
 <template>
-  <div style="height: 100%; padding: 10px;">
-    <no-ssr>
-      <l-map :options="mapOptions">
-        <l-image-overlay :url="url" :bounds="bounds" />
-      </l-map>
-    </no-ssr>
-  </div>
+  <no-ssr>
+    <l-map :options="mapOptions" style="height: 100%; padding: 10px; z-index: 0;">
+      <l-image-overlay
+        :url="overlayOptions.url"
+        :bounds="overlayOptions.bounds"
+      />
+    </l-map>
+  </no-ssr>
 </template>
 <script lang="ts">
 import Vue from 'vue'
@@ -29,17 +30,14 @@ export default Vue.extend({
           [1416, 512]
         ]
       },
-      url: 'https://i.ibb.co/bQYTGBR/floor.png',
-      bounds: [
-        [0, 0],
-        [1416, 512]
-      ]
+      overlayOptions: {
+        url: 'https://i.ibb.co/bQYTGBR/floor.png',
+        bounds: [
+          [0, 0],
+          [1416, 512]
+        ]
+      }
     }
-  },
-  created () {
-    const array = [1]
-    const other = Array.prototype.concat(array, 2, [3], [4])
-    console.log(other)
   }
 })
 </script>
