3a411181222fbf08ccbcbe1c86228ad188081b8a
Yevheniia Buzykina <yevheniia.buzykina@cytosmart.com>
2020-03-16 17:44:57 +0100

    working db context

---
+++ Froom/Froom.Data/FroomDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Froom.Data
+{
+    #region snippet_IDbContextFactory
+    public class FroomDbContextFactory : IDbContextFactory<FroomDbContext>
+    {
+        public FroomDbContext Create()
+        {
+            return new FroomDbContext();
+        }
+    }
+    #endregion
+}
---
+++ Froom/Froom/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Froom.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
