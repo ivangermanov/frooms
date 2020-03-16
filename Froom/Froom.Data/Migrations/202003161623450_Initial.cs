f07b34a8af5f0201b37833d650e883c21cb8d071
Yevheniia Buzykina <yevheniia.buzykina@cytosmart.com>
2020-03-16 17:44:57 +0100

    working db context

---
+++ Froom/Froom.Data/Migrations/202003161623450_Initial.cs
@@ -0,0 +1,43 @@
+﻿namespace Froom.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Reservations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        RoomName = c.String(unicode: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Number = c.Int(nullable: false),
+                        Name = c.String(unicode: false),
+                        Role = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Reservations", "UserId", "dbo.Users");
+            DropIndex("dbo.Reservations", new[] { "UserId" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Reservations");
+        }
+    }
+}
