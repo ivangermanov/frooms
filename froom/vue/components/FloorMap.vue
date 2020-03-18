abcd13b71a7ee08c4862896e32a1c19cc10ec677
Ivan Germanov <ivangermanov98@gmail.com>
2020-03-18 21:10:09 +0100

    Add first api endpoint

--- froom/vue/components/FloorMap.vue
+++ froom/vue/components/FloorMap.vue
@@ -1,6 +1,9 @@
 <template>
   <no-ssr>
-    <l-map :options="mapOptions" style="height: 100%; padding: 10px; z-index: 0;">
+    <l-map
+      :options="mapOptions"
+      style="height: 100%; padding: 10px; z-index: 0;"
+    >
       <l-image-overlay
         :url="overlayOptions.url"
         :bounds="overlayOptions.bounds"
@@ -9,16 +12,17 @@
   </no-ssr>
 </template>
 <script lang="ts">
-import Vue from 'vue'
-import { CRS } from 'leaflet'
-import { LMap, LImageOverlay } from 'vue2-leaflet'
+import Vue from "vue";
+import axios from "axios";
+import { CRS } from "leaflet";
+import { LMap, LImageOverlay } from "vue2-leaflet";
 
 export default Vue.extend({
   components: {
     LMap,
     LImageOverlay
   },
-  data () {
+  data() {
     return {
       mapOptions: {
         crs: CRS.Simple,
@@ -32,13 +36,18 @@ export default Vue.extend({
         ]
       },
       overlayOptions: {
-        url: 'https://i.ibb.co/bQYTGBR/floor.png',
+        url: "https://i.ibb.co/bQYTGBR/floor.png",
         bounds: [
           [0, 0],
           [1416, 512]
         ]
+      },
+      created() {
+        axios.get("weatherforecast").then(r => {
+          console.log(r);
+        });
       }
-    }
+    };
   }
-})
+});
 </script>
