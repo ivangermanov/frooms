7da91c281b15c967863f28d7b4aff8cdea72c14e
Anjela Melkonyan <anjela.melkonyan@cytosmart.com>
2020-03-24 21:12:33 +0100

    Added sample repository and interfaces

---
+++ WebAPI/Froom.Data/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,33 @@
+﻿using Froom.Data.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Froom.Data.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Adds a user to the database context.
+        /// </summary>
+        /// <param name="user"> The user to be added.</param>
+        Task<User> AddAsync(User user);
+
+        /// <summary>
+        /// Gets all users from the database context.
+        /// </summary>
+        IQueryable<User> GetAll();
+
+        /// <summary>
+        /// Gets a user by id from the database context.
+        /// </summary>
+        /// <param name="id"> The id of the user.</param>
+        Task<User> GetByIdAsync(Guid id);
+
+        /// <summary>
+        /// Updates information about a user.
+        /// </summary>
+        /// <param name="user"> The user to be updated.</param>
+        Task Update(User user);
+    }
+}
---
+++ WebAPI/Froom.Data/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Froom.Data.Database;
+using Froom.Data.Entities;
+using Froom.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Froom.Data.Repositories
+{
+    /// <inheritdoc cref="IUserRepository"/>
+    public class UserRepository : IUserRepository
+    {
+        private readonly DbSet<User> _users;
+        private readonly FroomContext _context;
+
+        public UserRepository(FroomContext context)
+        {
+            _context = context ?? throw new ArgumentException($"{nameof(FroomContext)} is null.");
+            _users = _context.Set<User>();
+        }
+
+        public Task<User> AddAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
