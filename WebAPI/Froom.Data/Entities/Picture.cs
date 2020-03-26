31aafc1f6c644ee544fa96ffaf4d1cef6a34b03e
Anjela Melkonyan <anjela.melkonyan@cytosmart.com>
2020-03-26 12:58:53 +0100

    Implemented entities, repository interfaces and added migrations

---
+++ WebAPI/Froom.Data/Entities/Picture.cs
@@ -0,0 +1,13 @@
+﻿namespace Froom.Data.Entities
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+
+        public int ReportId { get; set; }
+
+        public Report Report { get; set; }
+
+        public string Link { get; set; }
+    }
+}
