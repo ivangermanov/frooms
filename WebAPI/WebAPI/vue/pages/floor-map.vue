c5af86100d69788ad74b898cee0e5e43bc2675f5
Ivan Germanov <ivan.germanov@cytosmart.com>
2020-03-27 22:21:35 +0100

    Alert user before leaving with unsaved changes on drawing on floor map

--- WebAPI/WebAPI/vue/pages/floor-map.vue
+++ WebAPI/WebAPI/vue/pages/floor-map.vue
@@ -1,13 +1,30 @@
 <template>
-  <floor-map />
+  <floor-map :saved="saved" @save="saved=$event" />
 </template>
 
 <script>
-import FloorMap from '~/components/FloorMap.vue'
+import FloorMap from '@/components/FloorMap/FloorMap'
 
 export default {
   components: {
     FloorMap
+  },
+  data () {
+    return {
+      saved: true
+    }
+  },
+  beforeRouteLeave (_to, _from, next) {
+    if (!this.saved) {
+      const answer = window.confirm('You have unsaved changes! Do you really want to leave?')
+      if (answer) {
+        next()
+      } else {
+        next(false)
+      }
+    } else {
+      next()
+    }
   }
 }
 </script>
