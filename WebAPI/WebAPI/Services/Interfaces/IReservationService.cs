c7b3201e3020ed867cff20f9b8e5a0c73cff851c
Anjela Melkonyan <a.melkonyan@student.fontys.nl>
2020-04-01 23:22:46 +0200

    Implemented Room and Reservation services

---
+++ WebAPI/WebAPI/Services/Interfaces/IReservationService.cs
@@ -0,0 +1,29 @@
+﻿using Froom.Data.Dtos;
+using Froom.Data.Models.Reservations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Services.Interfaces
+{
+    public interface IReservationService
+    {
+        /// <summary>
+        /// Adds a new reservation.
+        /// </summary>
+        /// <param name="model">The reservation to be added.</param>
+        public Task AddReservationAsync(PostReservationModel model);
+
+        /// <summary>
+        /// Retrieves the reservations for specific room.
+        /// </summary>
+        /// <param name="roomId">The ID of the room.</param>
+        public IQueryable<ReservationDto> GetReservationsForRoom(int roomId);
+
+        /// <summary>
+        /// Retrieves the reservations for specific user.
+        /// </summary>
+        /// <param name="userId">The unique number of the user.</param>
+        /// <returns></returns>
+        public IQueryable<ReservationDto> GetReservationsForUser(int userNumber);
+    }
+}
--- WebAPI/WebAPI/Services/RoomService.cs
+++ WebAPI/WebAPI/Services/RoomService.cs
@@ -23,9 +23,7 @@ namespace WebAPI.Services
         public async Task AddRoomAsync(PostRoomModel model)
         {
             if (model is null)
-            {
                 throw new ArgumentException($"{nameof(PostRoomModel)} is null.");
-            }
 
             var newRoom = new Room
             {
@@ -40,18 +38,39 @@ namespace WebAPI.Services
             await _roomRepository.AddAsync(newRoom);
         }
 
-        public IQueryable<RoomDto> FilterRooms(string? campus, string? buildingName, int? floor)
+        public IQueryable<RoomDto> GetAvailableRooms(string campus, string buildingName, int floor, DateTime fromDate, DateTime toDate)
         {
-            var rooms = _roomRepository.GetAll()
-                .Where(r => r.Building.Campus == (campus ?? string.Empty))
-                .Where(r => r.BuildingName == (buildingName ?? string.Empty));
+            if (campus == null || buildingName == null)
+                throw new ArgumentNullException("The campus or the building name was null.");
 
-            if (floor == null)
-                return rooms.Select(r => new RoomDto(r, r.Building.Campus));
+            if (fromDate > toDate)
+                throw new ArgumentException("The start DateTime cannot be after the end DateTime.");
+
+            IQueryable<RoomDto> rooms = _roomRepository.GetAll()
+                                            .Where(r => r.Building.Campus == campus && 
+                                                   r.BuildingName == buildingName && 
+                                                   r.Floor == floor &&
+                                                   r.IsAvailable(fromDate, toDate))
+                                            .Select(r => new RoomDto(r));
+
+            return rooms;
+        }
+
+        public IQueryable<RoomDto> GetRooms(string campus, string buildingName, int? floor)
+        {
+            if (campus == null || buildingName == null)
+                throw new ArgumentNullException("The campus or the building name was null.");
+
+            IQueryable<Room> rooms = _roomRepository
+                                        .GetAll()
+                                        .Where(r => r.Building.Campus == campus && r.BuildingName == buildingName);
+
+            if (floor is null)
+                return rooms.Select(r => new RoomDto(r));
 
             return rooms
                 .Where(r => r.Floor == floor)
-                .Select(r => new RoomDto(r, r.Building.Campus));
+                .Select(r => new RoomDto(r));
         }
     }
 }
