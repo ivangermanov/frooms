d738dd48e3543d129bc8f4e7b754811385443ffa
Anjela Melkonyan <a.melkonyan@student.fontys.nl>
2020-04-01 20:40:12 +0200

    Added setup for tests and sample test for RoomRepository

---
+++ WebAPI/Froom.UnitTest/Helpers/SeededData.cs
@@ -0,0 +1,22 @@
+﻿using Froom.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froom.UnitTest.Helpers
+{
+    /// <summary>
+    /// Keeps sample data to be used in the unit tests
+    /// </summary>
+    public static class SeededData
+    {
+        public static User NormalUser { get; set; }
+
+        public static Building Building1 { get; set; }
+
+        /// <summary>
+        /// Room in Building1
+        /// </summary>
+        public static Room Room1 { get; set; }
+    }
+}
