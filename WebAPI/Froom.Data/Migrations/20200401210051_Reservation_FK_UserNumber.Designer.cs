d1f22425444ef18a79ad412f449cc77afd31ede2
Anjela Melkonyan <a.melkonyan@student.fontys.nl>
2020-04-01 23:03:50 +0200

    Entity changes and new migrations

--- WebAPI/Froom.Data/Migrations/20200401210051_Reservation_FK_UserNumber.Designer.cs
+++ WebAPI/Froom.Data/Migrations/20200401210051_Reservation_FK_UserNumber.Designer.cs
@@ -4,14 +4,16 @@ using Froom.Data.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Froom.Data.Migrations
 {
     [DbContext(typeof(FroomContext))]
-    partial class FroomContextModelSnapshot : ModelSnapshot
+    [Migration("20200401210051_Reservation_FK_UserNumber")]
+    partial class Reservation_FK_UserNumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebAPI/Froom.Data/Migrations/20200401210051_Reservation_FK_UserNumber.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Froom.Data.Migrations
+{
+    public partial class Reservation_FK_UserNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Users_UserId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Id_Number",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_UserId",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Reservations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserNumber",
+                table: "Reservations",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Users_Number",
+                table: "Users",
+                column: "Number");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Number",
+                table: "Users",
+                column: "Number",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_UserNumber",
+                table: "Reservations",
+                column: "UserNumber");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Users_UserNumber",
+                table: "Reservations",
+                column: "UserNumber",
+                principalTable: "Users",
+                principalColumn: "Number");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Users_UserNumber",
+                table: "Reservations");
+
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Users_Number",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Number",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_UserNumber",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "UserNumber",
+                table: "Reservations");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Reservations",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Id_Number",
+                table: "Users",
+                columns: new[] { "Id", "Number" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_UserId",
+                table: "Reservations",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Users_UserId",
+                table: "Reservations",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
