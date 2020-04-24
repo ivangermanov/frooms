9bcd789187abd579cc466b5fdf82460dd3160acc
Anjela Melkonyan <a.melkonyan@student.fontys.nl>
2020-04-24 15:45:19 +0200

    Add BuildingContents Repository

---
+++ WebAPI/Froom.Data/Repositories/Interfaces/IBuildingContentsRepository.cs
@@ -0,0 +1,17 @@
+﻿using Froom.Data.Entities;
+using System.Threading.Tasks;
+
+namespace Froom.Data.Repositories.Interfaces
+{
+    public interface IBuildingContentsRepository
+    {
+        /// <summary>
+        /// Gets the ID of the <see cref="BuildingContents"/> with the specific campus, building and floor.
+        /// </summary>
+        /// <param name="campusName">The name of the campus</param>
+        /// <param name="buildingName">The name of the building</param>
+        /// <param name="floorNumber">The floor number</param>
+        /// <returns></returns>
+        public Task<int> GetIdAsync(string campusName, string buildingName, string floorNumber);
+    }
+}
