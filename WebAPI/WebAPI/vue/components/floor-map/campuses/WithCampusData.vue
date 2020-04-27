0aee7f6d0c957b6e70d4a3874f97c2ed394125e5
Ivan Germanov <ivan.germanov@cytosmart.com>
2020-04-27 19:18:32 +0200

    Add WithCampusData and use-campus-data

---
+++ WebAPI/WebAPI/vue/components/floor-map/campuses/WithCampusData.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import Vue, { VNode } from 'vue'
+import { toRefs } from '@vue/composition-api'
+import useCampusData from '@/composition/use-campus-data'
+
+export default Vue.extend({
+  props: {
+    campusName: { default: 'EHV', type: String },
+    buildingName: { default: 'R1', type: String },
+    floorNumber: { default: 'bg', type: String }
+  },
+  setup () {
+    const data = useCampusData()
+
+    data.getCampusNames()
+
+    return { ...toRefs(data) }
+  },
+  render (): VNode {
+    const { campusNames } = this
+
+    return this.$scopedSlots.default!({
+      campusNames
+    }) as any
+  }
+})
+</script>
