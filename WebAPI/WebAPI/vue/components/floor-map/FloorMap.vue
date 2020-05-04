ddd07ccba4a27b2937dca5e22562fc2c8c4897ab
Ivan Germanov <ivangermanov98@gmail.com>
2020-05-04 21:35:20 +0200

    Pass campusName, buildingName and floorNumber to WithRoomsData

--- WebAPI/WebAPI/vue/components/floor-map/FloorMap.vue
+++ WebAPI/WebAPI/vue/components/floor-map/FloorMap.vue
@@ -6,65 +6,49 @@
       style="height: 100%; z-index: 0;"
     >
       <l-control-zoom position="topright" />
-      <with-campus-data
-        v-slot="{
-          campusNames
-        }"
+
+      <with-room-data
+        :campus-name="campusName"
+        :building-name="buildingName"
+        :floor-number="floorNumber"
       >
-        <template>
-          <with-building-data
-            v-slot="{
-              buildings
-            }"
-            :campus-name="campusName"
-          >
-            <template>
-              <with-room-data
-                :campus-name="campusName"
-                :building-name="buildingName"
-                :floor-number="floorNumber"
-              >
-                <template
-                  v-slot="{
-                    roomLayers,
-                    postShape,
-                    putShapes,
-                    deleteShapes
-                  }"
-                >
-                  <div v-if="mapObject">
-                    <l-campus-control
-                      :campus-name.sync="campusName"
-                      :campuses="campusNames"
-                      position="topleft"
-                    />
-                    <l-building-control
-                      :building-name.sync="buildingName"
-                      :buildings="buildings.map(building => building.name)"
-                      position="topleft"
-                    />
-                    <l-floors-control
-                      :campus-names="campusNames"
-                      :buildings="buildings"
-                      :map-object="mapObject"
-                      position="bottomright"
-                      :floor-number.sync="floorNumber"
-                    />
-                    <l-draw
-                      position="topright"
-                      :map-object="mapObject"
-                      :fetched-layers="roomLayers"
-                      @addLayer="postShape"
-                      @editLayers="putShapes"
-                      @deleteLayers="deleteShapes"
-                    />
-                  </div>
-                </template>
-              </with-room-data>
-            </template>
-          </with-building-data>
+        <template
+          v-slot="{
+            roomLayers,
+            postShape,
+            putShapes,
+            deleteShapes
+          }"
+        >
+          <div v-if="mapObject">
+            <l-campus-control
+              :campus-name.sync="campusName"
+              :campuses="campusNames"
+              position="topleft"
+            />
+            <l-building-control
+              :building-name.sync="buildingName"
+              :buildings="buildings.map(building => building.name)"
+              position="topleft"
+            />
+            <l-floors-control
+              :campus-names="campusNames"
+              :buildings="buildings"
+              :map-object="mapObject"
+              position="bottomright"
+              :floor-number.sync="floorNumber"
+            />
+            <l-draw
+              position="topright"
+              :map-object="mapObject"
+              :fetched-layers="roomLayers"
+              @addLayer="postShape"
+              @editLayers="putShapes"
+              @deleteLayers="deleteShapes"
+            />
+          </div>
         </template>
-      </with-campus-data>
+      </with-room-data>
     </l-map>
   </client-only>
 </template>
@@ -74,13 +58,14 @@ import Vue from 'vue'
 import { CRS, Map } from 'leaflet'
 import { LMap, LControlZoom } from 'vue2-leaflet'
 
-import WithCampusData from './campuses/WithCampusData.vue'
-import WithBuildingData from './buildings/WithBuildingData.vue'
-import WithRoomData from './rooms/WithRoomData.vue'
+import { toRefs } from '@vue/composition-api'
 import LDraw from './rooms/draw/LDraw.vue'
 import LFloorsControl from './rooms/floors/LFloorsControl.vue'
 import LCampusControl from './campuses/LCampusControl.vue'
 import LBuildingControl from './buildings/LBuildingControl.vue'
+import WithRoomData from './rooms/WithRoomData.vue'
+import useCampusData from '@/composition/use-campus-data'
+import useBuildingData from '@/composition/use-building-data'
 
 export default Vue.extend({
   components: {
@@ -90,8 +75,6 @@ export default Vue.extend({
     LCampusControl,
     LBuildingControl,
     LFloorsControl,
-    WithCampusData,
-    WithBuildingData,
     WithRoomData
   },
   data () {
@@ -108,6 +91,29 @@ export default Vue.extend({
       campusName: '',
       buildingName: '',
       floorNumber: ''
+    }
+  },
+  computed: {
+    buildingNames () {
+      const { buildings } = this as any
+      return buildings.map((building: any) => building.name)
+    }
+  },
+  setup () {
+    const campusData = useCampusData()
+    campusData.getCampusNames()
+
+    const buildingData = useBuildingData()
+    buildingData.getBuildings()
+
+    return { ...toRefs(campusData), ...toRefs(buildingData) }
+  },
+  watch: {
+    campusNames (value) {
+      if (this.campusName === '') { this.campusName = value[0] }
+    },
+    buildings (value) {
+      if (this.buildingName === '') { this.buildingName = value[0].name }
     }
   },
   mounted () {
