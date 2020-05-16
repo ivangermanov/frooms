45e7f2f549adf9fc243c19a8d242f0972f8b972f
Anjela Melkonyan <a.melkonyan@student.fontys.nl>
2020-05-16 11:48:35 +0200

    Add Notification entity

---
+++ WebAPI/Froom.Data/Entities/Notification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Froom.Data.Entities
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
