967c9a11235f8e0d6d17486f2e8de6126fffb72e
Anjela Melkonyan <a.melkonyan@student.fontys.nl>
2020-05-16 11:50:17 +0200

    Add notification Dto and PostModel

---
+++ WebAPI/Froom.Data/Models/Notifications/PostNotificationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Froom.Data.Models.Notifications
+{
+    public class PostNotificationModel
+    {
+        public Guid UserId { get; set; }
+
+        public string Message { get; set; }
+    }
+}
--- WebAPI/Froom.Data/Models/Users/PostUserModel.cs
+++ WebAPI/Froom.Data/Models/Users/PostUserModel.cs
@@ -1,11 +1,11 @@
-﻿using Froom.Data.Entities;
-using System;
+﻿using System;
 
 namespace Froom.Data.Models.Users
 {
     public class PostUserModel
     {
         public Guid Id { get; set; }
+
         public string Name { get; set; }
     }
 }
