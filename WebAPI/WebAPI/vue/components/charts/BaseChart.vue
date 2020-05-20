c69efe963b3347a13a54783ed3d2b4dff7c3d94c
Chan,Jeffrey J.K.B <jeffrey.chan@student.fontys.nl>
2020-05-20 21:56:50 +0200

    Add nuxt user middleware

---
+++ WebAPI/WebAPI/vue/components/charts/BaseChart.vue
@@ -0,0 +1,64 @@
+<template>
+  <chart :options="chartOptions" />
+</template>
+
+<script>
+import { Chart } from 'highcharts-vue'
+
+export default {
+  components: { Chart },
+
+  data () {
+    return {
+      chartOptions: {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Monthly Room Reservations'
+        },
+        subtitle: {
+          text: 'Source: Froom Application'
+        },
+        xAxis: {
+          categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+          ],
+          crosshair: true
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Reservations made'
+          }
+        },
+        plotOptions: {
+          column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+          }
+        },
+        series: [{
+          name: 'Reservations',
+          data: [49, 71, 106, 129, 144, 176, 135, 148, 216, 194, 95, 54]
+
+        }],
+        credits: {
+          enabled: false
+        }
+      }
+    }
+  }
+}
+</script>
