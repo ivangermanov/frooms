faa6a4ab01611ee554d3eff29590279a46d9cc12
Yevheniia Buzykina <yevheniia.buzykina@cytosmart.com>
2020-05-26 15:46:43 +0200

    working on dialog

--- WebAPI/WebAPI/vue/components/admin/user-overview/UserOverview.vue
+++ WebAPI/WebAPI/vue/components/admin/user-overview/UserOverview.vue
@@ -1,14 +1,38 @@
 <template>
   <v-data-table
-      :headers="headers"
-      :items="users"
-      sort-by="Name"
-      class="elevation-1"
+    :headers="headers"
+    :items="users"
+    sort-by="Name"
+    class="elevation-1"
   >
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline"> Edit the roles</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+            hey
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="blue darken-1" text @click="close">
+            Cancel
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="save">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <template v-slot:item.actions="{ item }">
       <v-icon
         small
         class="mr-2"
+        @click="editUser(item)"
       >
         mdi-pencil
       </v-icon>
@@ -44,11 +68,15 @@ export default {
   data () {
     return {
       dialog: false,
+      roles: {
+        isAdmin: false,
+        isUser: false
+      },
       headers: [
         {
           text: 'Name',
           align: 'start',
-          value: 'name',
+          value: 'name'
         },
         { text: 'Email', value: 'email' },
         { text: 'Role', value: 'role' },
@@ -63,13 +91,28 @@ export default {
   },
   methods: {
     async blockUser (user) {
-      const { data } = await AdminUserRepository.blockUser(user.id);
-      user.isBlocked = data.isBlocked;
+      if (confirm('Are you sure you want to block this user?')) {
+        const { data } = await AdminUserRepository.blockUser(user.id)
+        user.isBlocked = data.isBlocked
+      }
     },
     async unblockUser (user) {
-      const { data } = await AdminUserRepository.unblockUser(user.id);
-      user.isBlocked = data.isBlocked;
+      if (confirm('Are you sure you want to unblock this user?')) {
+        const { data } = await AdminUserRepository.unblockUser(user.id)
+        user.isBlocked = data.isBlocked
+      }
     },
+    editUser (roles) {
+      console.log(roles)
+      this.dialog = true
+    },
+    close () {
+      this.dialog = false
+    },
+
+    save () {
+      this.close()
+    }
   }
 }
 </script>
