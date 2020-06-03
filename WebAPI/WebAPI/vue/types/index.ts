5a2e63ac286af00ebdc9420e45522c7570c70c4c
Ivan Germanov <ivan.germanov@cytosmart.com>
2020-06-03 20:15:23 +0200

    Fix UserRoles enum

--- WebAPI/WebAPI/vue/types/index.ts
+++ WebAPI/WebAPI/vue/types/index.ts
@@ -1,4 +1,4 @@
 export { RoomDTO, IRoomDTO, IFloormapRoomDTO, RoomModel, IRoomModel, IPoint, CreateIRoomModel, IRoomToGeoJSONFeature } from './room'
 export { IFloor } from './floor'
-export { IUser, IPostUser, UserRoles } from './user'
+export { IUser, IPostUser, UserRole } from './user'
 export { IReservation } from './reservation'
