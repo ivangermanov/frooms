9555a7acb623a7772f6fa46b50288decc202abe9
Anjela Melkonyan <a.melkonyan@student.fontys.nl>
2020-06-03 15:41:38 +0200

    Disable blocking admins in back-end

--- WebAPI/WebAPI/Services/UserService.cs
+++ WebAPI/WebAPI/Services/UserService.cs
@@ -114,18 +114,22 @@ namespace WebAPI.Services
         public async Task<UserDto> BlockUserAsync(Guid id)
         {
             var user = await _userRepository.GetByIdAsync(id);
-            user.IsBlocked = true;
-            await _userRepository.Update(user);
 
-            var notification = new Notification()
+            if(user.Role != UserRole.ADMIN)
             {
-                UserId = user.Id,
-                Title = "Profile blocked",
-                Message = "You are now blocked. " +
-                "Contact the administration for more information."
-            };
+                user.IsBlocked = true;
+                await _userRepository.Update(user);
 
-            await _notificationRepository.AddAsync(notification);
+                var notification = new Notification()
+                {
+                    UserId = user.Id,
+                    Title = "Profile blocked",
+                    Message = "You are now blocked. " +
+                    "Contact the administration for more information."
+                };
+
+                await _notificationRepository.AddAsync(notification);
+            }
 
             return _mapper.Map<UserDto>(user);
         }
