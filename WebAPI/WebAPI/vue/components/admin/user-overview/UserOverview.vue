6ffd27b5e152a2fdaac074a2e6dcfdeb304c50ef
Chan, J.K.B <397773@student.fontys.nl>
2020-06-12 20:33:58 +0200

    Add menu to navigation, Fix column breakpoints UsersOverview

--- WebAPI/WebAPI/vue/components/admin/user-overview/UserOverview.vue
+++ WebAPI/WebAPI/vue/components/admin/user-overview/UserOverview.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <v-col cols="8">
+      <v-col cols="12" md="8">
         <v-card>
           <v-card-title>
             <v-text-field
--- WebAPI/WebAPI/vue/layouts/admin.vue
+++ WebAPI/WebAPI/vue/layouts/admin.vue
@@ -22,43 +22,50 @@
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <div class="d-flex mr-4">
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <span class="d-flex" v-on="on">
-              <v-icon class="pr-2">
-                mdi-theme-light-dark
+      <v-menu offset-y :close-on-content-click="false">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon class="pr-2">
+              mdi-account
+            </v-icon>
+            {{ getGivenName }}
+          </v-btn>
+        </template>
+        <v-list dense>
+          <v-list-item>
+            <v-list-item-content class="pa-0">
+              <v-switch
+                v-model="$vuetify.theme.dark"
+                prepend-icon="mdi-theme-light-dark"
+                class="pa-0"
+              />
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/">
+            <v-list-item-icon>
+              <v-icon>
+                mdi-home
               </v-icon>
-              <v-switch v-model="$vuetify.theme.dark" class="switch" />
-            </span>
-          </template>
-          <span>Dark mode</span>
-        </v-tooltip>
-      </div>
-      <v-tooltip v-if="true" bottom>
-        <template v-slot:activator="{ on }">
-          <span class="d-flex" v-on="on">
-            <v-btn to="/">
-              <v-icon class="pr-2">mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
               Home
-            </v-btn>
-          </span>
-        </template>
-        <span>Home</span>
-      </v-tooltip>
-      <v-tooltip bottom>
-        <template v-slot:activator="{ on }">
-          <span class="d-flex ml-2" v-on="on">
-            <a href="/api/logout">
-              <v-btn>
-                <v-icon class="pr-2">mdi-logout</v-icon>
-                Logout
-              </v-btn>
-            </a>
-          </span>
-        </template>
-        <span>Logout</span>
-      </v-tooltip>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/api/logout">
+            <v-list-item-icon>
+              <v-icon>
+                mdi-logout
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              Logout
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
     <v-content>
       <nuxt />
@@ -67,6 +74,7 @@
 </template>
 
 <script lang="ts">
+import { mapGetters } from 'vuex'
 
 export default {
   middleware: ['user', 'admin'],
@@ -81,15 +89,14 @@ export default {
         }
       ],
       miniVariant: false,
-      title: 'Froom Admin Dashboard'
+      title: 'Froom'
     }
+  },
+
+  computed: {
+    ...mapGetters({
+      getGivenName: 'user/getGivenName'
+    })
   }
 }
 </script>
-
-<style scoped>
-  .switch {
-    height: 25px;
-    align-items: initial;
-  }
-</style>
