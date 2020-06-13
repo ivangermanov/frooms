db251f089cb12c2d22d91e52aa8b647b88e5762c
Ivan Germanov <ivan.germanov@cytosmart.com>
2020-06-13 19:06:08 +0200

    Create ReservationRulesDto

---
+++ WebAPI/Froom.Data/Dtos/ReservationRulesDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Froom.Data.Dtos
+{
+    public class ReservationRulesDto
+    {
+        public DateTime CurrentDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DayOfWeek[] AvailableDays { get; set; }
+    }
+}
