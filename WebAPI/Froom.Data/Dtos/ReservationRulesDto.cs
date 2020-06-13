b721f7cae8479fee03714da37bfb6f859679a61e
Ivan Germanov <ivangermanov98@gmail.com>
2020-06-13 20:55:35 +0200

    Rename StartTime to MinTime and EndTime to MaxTime

--- WebAPI/Froom.Data/Dtos/ReservationRulesDto.cs
+++ WebAPI/Froom.Data/Dtos/ReservationRulesDto.cs
@@ -5,8 +5,8 @@ namespace Froom.Data.Dtos
     public class ReservationRulesDto
     {
         public DateTime CurrentDate { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public DateTime MinTime { get; set; }
+        public DateTime MaxTime { get; set; }
         public DayOfWeek[] AvailableDays { get; set; }
     }
 }
--- WebAPI/WebAPI/Helpers/ReservationRules.cs
+++ WebAPI/WebAPI/Helpers/ReservationRules.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;
 
 namespace WebAPI.Helpers
 {
     public static class ReservationRules
     {
         public static DateTime CurrentDate => DateTime.Today;
-        public static DateTime StartTime => DateTime.Today.AddHours(9);
-        public static DateTime EndTime => DateTime.Today.AddHours(17);
+        public static DateTime MinTime => DateTime.Today.AddHours(9);
+        public static DateTime MaxTime => DateTime.Today.AddHours(17);
         public static DayOfWeek[] AvailableDays => new[]
         {
             DayOfWeek.Monday,
