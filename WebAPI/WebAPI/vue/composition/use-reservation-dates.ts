74d68c8a4b9ae3d118912a74181285d0be2dbfc6
Ivan Germanov <ivangermanov98@gmail.com>
2020-06-13 22:01:39 +0200

    Integrate back-end reservation rules with use-reservation-dates

--- WebAPI/WebAPI/vue/composition/use-reservation-dates.ts
+++ WebAPI/WebAPI/vue/composition/use-reservation-dates.ts
@@ -1,53 +1,58 @@
 import moment from 'moment'
-import { reactive, ref, toRefs, computed, onUnmounted, watch } from '@vue/composition-api'
+import { reactive, ref, toRefs, computed, onUnmounted, watch, onMounted } from '@vue/composition-api'
+import { RepositoryFactory } from '@/api/repositoryFactory'
+const ReservationRepository = RepositoryFactory.reservation
 
 export default function useRoomsData () {
-  // TODO: Local time needs to be retrieved from back-end or set as GMT +1 (Netherlands)
   const currentDate = ref(moment())
-  // TODO: Get start time and end time for reservations from back-end business logic
-  const minTime = moment('9:00', 'HH:mm')
-  const maxTime = moment('17:00', 'HH:mm')
+  const minTime = ref(moment('9:00', 'HH:mm'))
+  const maxTime = ref(moment('17:00', 'HH:mm'))
+  // 15 minutes
+  const minReservationTime = ref(900000)
+  // 3 hours
+  const maxReservationTime = ref(10800000)
+  // 3 months
+  const maxForwardReservationPeriod = ref(7776000000)
+
   const data = reactive({
-    date: currentDate.value.format('YYYY-MM-DD') as string,
-    startTime: currentDate.value.format('HH:mm') as string,
-    // TODO: Get max reservation time from back-end business logic
+    date: currentDate.value.format('YYYY-MM-DD'),
+    startTime: currentDate.value.format('HH:mm'),
     endTime: currentDate.value
       .clone()
-      .add(3, 'hour')
-      .format('HH:mm') as string
+      .add(maxReservationTime.value, 'milliseconds')
+      .format('HH:mm')
   })
 
   const minDate = computed(() => {
-    if (currentDate.value.isAfter(maxTime)) {
+    if (currentDate.value.isAfter(maxTime.value)) {
       return currentDate.value.clone().add(1, 'day').format('YYYY-MM-DD')
     }
     return currentDate.value.format('YYYY-MM-DD')
   })
   const maxDate = computed(() =>
-  // TODO: Get months in advance from back-end business logic
-    currentDate.value.clone().add(3, 'M').format('YYYY-MM-DD')
+    currentDate.value.clone()
+      .add(maxForwardReservationPeriod.value, 'milliseconds')
+      .format('YYYY-MM-DD')
   )
 
   const minStart = computed(() => {
-    if (startDateMoment.value.isAfter(maxTime) || startDateMoment.value.isBefore(minTime)) {
-      return minTime.format('HH:mm')
+    if (startDateMoment.value.isAfter(maxTime.value) || startDateMoment.value.isBefore(minTime.value)) {
+      return minTime.value.format('HH:mm')
     }
     return currentDate.value.format('HH:mm')
   })
   const maxStart = computed(() =>
-  // TODO: Get max from back-end business logic
-    maxTime.clone().subtract(15, 'minutes').format('HH:mm')
+    maxTime.value.clone().subtract(minReservationTime.value, 'milliseconds').format('HH:mm')
   )
 
   const minEnd = computed(() => {
-    // TODO: Get min hours for booking from back-end business logic
-    const startOffset = moment(data.startTime, 'HH-mm').add(15, 'minutes')
-    return startOffset.isAfter(maxTime) ? maxTime.format('HH:mm') : startOffset.format('HH:mm')
+    const startOffset = moment(data.startTime, 'HH-mm').add(minReservationTime.value, 'milliseconds')
+    return startOffset.isAfter(maxTime.value) ? maxTime.value.format('HH:mm') : startOffset.format('HH:mm')
   }
   )
   const maxEnd = computed(() => {
-    const startOffset = moment(data.startTime, 'HH-mm').add(3, 'hours')
-    return startOffset.isAfter(maxTime) ? maxTime.format('HH:mm') : startOffset.format('HH:mm')
+    const startOffset = moment(data.startTime, 'HH-mm').add(maxReservationTime.value, 'milliseconds')
+    return startOffset.isAfter(maxTime.value) ? maxTime.value.format('HH:mm') : startOffset.format('HH:mm')
   })
 
   const startDateMoment = computed(() => moment(`${data.date} ${data.startTime}`))
@@ -87,11 +92,20 @@ export default function useRoomsData () {
     }, remaining)
   }
 
+  onMounted(async () => {
+    const { data } = await ReservationRepository.getRules()
+    currentDate.value = moment(data.currentDate)
+    minTime.value = moment(data.minTime)
+    maxTime.value = moment(data.maxTime)
+    minReservationTime.value = data.minReservationTime
+    maxReservationTime.value = data.maxReservationTime
+    clearTimeout(timer)
+    updateCurrentDate()
+  })
+
   onUnmounted(() => {
     clearTimeout(timer)
   })
-
-  updateCurrentDate()
 
   return {
     ...toRefs(data),
