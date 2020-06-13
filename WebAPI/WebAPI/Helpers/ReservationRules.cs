28e968df78b9e23a62ee962f33e4637cc1459d5d
Ivan Germanov <ivangermanov98@gmail.com>
2020-06-13 18:50:30 +0200

    Create ReservationRules.cs

---
+++ WebAPI/WebAPI/Helpers/ReservationRules.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebAPI.Helpers
+{
+    public class ReservationRules
+    {
+        public DateTime CurrentDate => DateTime.Today;
+        public DateTime StartTime => DateTime.Today.AddHours(9);
+        public DateTime EndTime => DateTime.Today.AddHours(17);
+
+        public DayOfWeek[] AvailableDays => new[]
+        {
+            DayOfWeek.Monday,
+            DayOfWeek.Tuesday,
+            DayOfWeek.Wednesday,
+            DayOfWeek.Thursday,
+            DayOfWeek.Friday
+        };
+    }
+}
