5ec1ecbcf496bfa2205dfdc088b25ba6936d4d9a
Ivan Germanov <ivan.germanov@cytosmart.com>
2020-06-14 15:51:11 +0200

    Add TypeScript DayOfWeek enum

--- WebAPI/WebAPI/vue/types/reservation/index.ts
+++ WebAPI/WebAPI/vue/types/reservation/index.ts
@@ -1,3 +1,4 @@
 export { IReservationAdmin } from './IReservationAdmin'
 export { IReservation } from './IReservation'
 export { IReservationRules } from './IReservationRules'
+export { DayOfWeek } from './DayOfWeek'
